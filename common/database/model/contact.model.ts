791f3e09044761490cb2a453c86522495ee5f733
maceteligolden <maceteligolden@gmail.com>
2023-07-29 20:53:55 +0100

    chore: moving right files

--- common/database/model/contact.model.ts
+++
@@ -1,6 +0,0 @@
-import Base from "./base.model";
-
-export default interface Contact extends Base {
-    owner_id?: string;
-    contact_id?: string;
-}
--- common/database/model/user.model.ts
+++
@@ -1,9 +0,0 @@
-import Base from "./base.model";
-
-export default interface User extends Base {
-    name?: string;
-    phone?: number;
-    avatar?: string;
-    status?: string;
-    about?: string;
-}
